efc715275fdb037c2e880d5583051bf375cca5e9
Vikas Godase <vikasgodase11@gmail.com>
2026-07-31 12:34:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -1,9 +1,9 @@
 class Solution {
     int factorial(int n) {
-        int f = 1;
-        for(int i=1; i<=n; i++){
-            f = f * i;
+        // code here
+        if(n == 0 || n == 1){
+            return 1;
         }
-        return f;
+        return n * factorial(n - 1);
     }
 }
